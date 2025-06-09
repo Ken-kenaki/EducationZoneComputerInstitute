28deeeddb1a2f3a825aa4aca52093b2b09a8ecbf
Ken-kenaki <kenofficial588@gmail.com>
2025-06-10 01:21:27 +0545

    eslint-error-fixed-5

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { logout } from "../logout/actions";
 
 const dahboard = () => {
   return (
     <div className="mt-100">
       dahboard
-      <form action={logout}>
+      {/* <form action={logout}>
         <button type="submit">Log Out</button>
-      </form>
+      </form> */}
     </div>
   );
 };
